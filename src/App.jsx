055e30d8e36364b76eb8294e6fa9648407fe5bae
Ashish Prajapati <116935348+Anticoder03@users.noreply.github.com>
2025-11-20 00:25:44 +0530

    ready

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+import { useState } from 'react';
+export const Component = () => {
+  return(
+    <div>
+    <h1>hello world</h1>
+    </div>
+  )
+}
+
+export default Component
